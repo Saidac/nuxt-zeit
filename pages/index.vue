3f57279de74c1fb59e77631bd9dfaff7f1906aa5
William Saidac <wsaidac@cg-mb2018046.local>
2019-10-15 11:33:15 +0200

    fixed linting issues

--- pages/index.vue
+++ pages/index.vue
@@ -9,11 +9,7 @@
         Practice deploying nuxt app with zeit
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
